0b189db63db8adde199b34c0b33d5840db665201
Piotr Kupczyk <piotr.kupczyk97@gmail.com>
2019-09-23 18:23:02 +0200

    Init

---
+++ src/main/kotlin/com/example/billspliter/BillSpliterApplication.kt
@@ -0,0 +1,11 @@
+package com.example.billspliter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BillSpliterApplication
+
+fun main(args: Array<String>) {
+	runApplication<BillSpliterApplication>(*args)
+}
